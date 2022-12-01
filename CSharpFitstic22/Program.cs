77d27b8018e2c8561a12485ab45265d1ea1958d5
Crippao <cristina.paoloni@outlook.com>
2022-12-01 12:16:43 +0100

    Commit Es3B

---
+++ CSharpFitstic22/Program.cs
@@ -0,0 +1,23 @@
+﻿using Es3B;
+
+namespace CSharpFitstic22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Che esrcizio vuoi caricare? Inserire numero: ");
+            Console.WriteLine("1. Esercizio 3B");
+
+            string? temp = Console.ReadLine();
+            int.TryParse(temp, out int e);
+
+            if (e == 1)
+            {
+                new Exercise().Execute();
+            }
+
+
+        }
+    }
+}
---
+++ Es3B/Exercise.cs
@@ -0,0 +1,27 @@
+﻿namespace Es3B
+{
+    public class Exercise
+    {
+        public void Execute()
+        {
+            string tmp;
+            bool ok;
+
+
+            Console.Write("Inserisci un numero: ");
+            tmp = Console.ReadLine();
+            ok = int.TryParse(tmp, out int numero);
+
+            if (numero % 2 == 0)
+            {
+                numero--;
+            }
+
+            while (numero > 0)
+            {
+                Console.WriteLine(numero);
+                numero = numero - 2;
+            }
+        }
+    }
+}
