74712d48a1618e426c748a7ab098bc2322f07d90
Cristina <109718308+Crippao@users.noreply.github.com>
2023-01-24 10:16:57 +0100

    Add files via upload
    Add bubble sort

---
+++ bubble_sort/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ordina_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri;
+            bool ordered;
+
+            numeri = ChiediNumRandom();
+
+            for (int i = 0; i < numeri.Length; i++)
+            {
+                Console.WriteLine($"L'array creato è: {numeri[i]}");
+            }
+
+            do
+            {
+                ordered = true;
+
+                for (int i = 0; i < numeri.Length - 1; i++)
+                {
+                    if (Ordina(ref numeri[i], ref numeri[i + 1]))
+                    {
+                        ordered = false;
+
+                    }
+                }
+            } while (!ordered);
+
+            for (int i = 0; i < numeri.Length; i++)
+            {
+
+                Console.WriteLine($"L'array ordinato è: {numeri[i]}");
+            }
+
+        }
+
+        static void ChiediNum(string richiesta, ref int n)
+        {
+            do
+            {
+                Console.WriteLine($"Inserisci {richiesta}: ");
+
+            } while (!int.TryParse(Console.ReadLine(), out n));
+        }
+
+        static int[] ChiediNumRandom()
+        {
+            int indice = 0;
+            Random r = new Random();
+
+            ChiediNum("il totale di numeri da inserire", ref indice);
+            int[] array = new int[indice];
+
+            for (int i = 0; i < indice; i++)
+            {
+                array[i] = r.Next(0, 100);
+            }
+            return array;
+        }
+
+        static bool Ordina(ref int num, ref int num2)
+        {
+            bool ok = false;
+
+            if (num >= num2)
+            {
+                int temp = num;
+                num = num2;
+                num2 = temp;
+                ok = true;
+            }
+
+            return ok;
+        }
+    }
+}
